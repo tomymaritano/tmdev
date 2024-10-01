282690dff7b115a87279d532f4fd0beff994dd66
Tomás Maritano <tomymaritano@gmail.com>
2024-09-30 23:24:17 -0300

    page updates

--- pages/works/unicoin.js
+++ pages/works/unicoin.js
@@ -5,13 +5,12 @@ import {
   Link,
   List,
   ListItem,
-  // SimpleGrid,
   UnorderedList,
   Image
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
-import { Title, WorkImage, Meta } from '../../components/work'
+import { Title,  Meta } from '../../components/work'
 import P from '../../components/paragraph'
 
 const Work = () => (
